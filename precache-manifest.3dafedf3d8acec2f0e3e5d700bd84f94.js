9dc14bf6516110953a2594768b47d5e9312abcbd
MBY90 <med.ben.youssefisi@gmail.com>
2020-06-22 14:30:59 +0100

    Updates

---
+++ precache-manifest.3dafedf3d8acec2f0e3e5d700bd84f94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d2246802471e562cb3913db316d52d",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "2555c5f9dfcbd5a07227",
+    "url": "/food-recipe/static/css/main.3f92da09.chunk.css"
+  },
+  {
+    "revision": "1093d2036f75b13ef983",
+    "url": "/food-recipe/static/js/2.585003c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe/static/js/2.585003c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2555c5f9dfcbd5a07227",
+    "url": "/food-recipe/static/js/main.6ff6d310.chunk.js"
+  },
+  {
+    "revision": "313cc743206844ef4a2c",
+    "url": "/food-recipe/static/js/runtime-main.559844aa.js"
+  }
+]);
